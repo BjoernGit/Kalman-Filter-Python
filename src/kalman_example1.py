1dc0e3e8cf2e07a049bc4a77521bea533061f34f
BjoernGit <49546621+BjoernGit@users.noreply.github.com>
2025-08-10 00:15:12 +0200

    filter plot implemented

--- src/kalman_example1.py
+++ src/kalman_example1.py
@@ -23,36 +23,32 @@ def kfilter(z, updateNumber):
   sd = 10
 
   if updateNumber == 1:
-    kfilter.x = np.array([[0],
-                          [20]])
+    kfilter.x = np.array([[0],[20]])
     kfilter.P = np.array([[5,0],
                           [0,5]])
     kfilter.A = np.array([[1,dt],
                           [0,1]])
-    kfilter.H = np.array([[1],
-                          [0]])
+    kfilter.H = np.array([[1,0]])
     kfilter.HT = np.array([[1],
                            [0]])
     kfilter.R = 10
     kfilter.Q = sd * np.array([[dt**3/3,dt**2],[dt**2,dt]])
 
-    #predict state forward
-    x_prime = kfilter.A.dot(kfilter.x)
-    #predict covariance forward
-    P_prime = kfilter.A.dot(kfilter.P).dot(kfilter.A.T) + kfilter.Q
+  #predict state forward
+  x_prime = kfilter.A.dot(kfilter.x)
+  #predict covariance forward
+  P_prime = kfilter.A.dot(kfilter.P).dot(kfilter.A.T) + kfilter.Q
     
-    #compute kalman gain
-    S = kfilter.H.dot(P_prime).dot(kfilter.HT) + kfilter.R
-    K = P_prime.dot(kfilter.HT) * (1/S)
+  #compute kalman gain
+  S = kfilter.H.dot(P_prime).dot(kfilter.HT) + kfilter.R
+  K = P_prime.dot(kfilter.HT) * (1/S)
 
-    #estimate state
-    residual = z - kfilter.H.dot(x_prime)
-    kfilter.x = x_prime + K*residual
-
-    #estimate covariance
-    kfilter.P = P_prime - K.dot(kfilter.H).dot(P_prime)
-
-    return [kfilter.x[0], kfilter.x[1], kfilter.P,K]
+  #estimate state
+  residual = z - kfilter.H.dot(x_prime)
+  kfilter.x = x_prime + K*residual
+  #estimate covariance
+  kfilter.P = P_prime - K.dot(kfilter.H).dot(P_prime)
+  return [kfilter.x[0], kfilter.x[1], kfilter.P,K]
 
 def testFilter():  
   #define range of measurements to loop
@@ -77,7 +73,8 @@ def testFilter():
     z=getMeasurement(k)
     #call filter and return new state
     f = kfilter(z[0],k)
-    #save off that state so that it could be plotted meas Time.append(k)
+    #save off that state so that it could be plotted 
+    measTime.append(k)
     measPos.append(z[0])
     measDifPos.append(z[0]-z[1])
     estDifPos.append(f[0]-z[1])
@@ -90,3 +87,42 @@ def testFilter():
     velGain.append(K[1][0])
 
   return [measTime,measPos,estPos,estVel,measDifPos,estDifPos,posBound3Sigma,posGain,velGain];
+
+# Execute Test Filter Function
+t = testFilter()
+# Plot Results
+plot1 = plt.figure(1)
+plt.scatter(t[0], t[1])
+plt.plot(t[0], t[2])
+plt.ylabel('Position')
+plt.xlabel('Time')
+plt.grid(True)
+
+plot2 = plt.figure(2)
+plt.plot(t[0], t[3])
+plt.ylabel('Velocity (meters/seconds)')
+plt.xlabel('Update Number')
+plt.title('Velocity Estimate On Each Measurement Update \n', fontweight="bold")
+plt.legend(['Estimate'])
+plt.grid(True)
+
+plot3 = plt.figure(3)
+plt.scatter(t[0], t[4], color = 'red')
+plt.plot(t[0], t[5])
+plt.legend(['Estimate', 'Measurement'])
+plt.title('Position Errors On Each Measurement Update \n', fontweight="bold")
+plt.ylabel('Position Error (meters)')
+plt.xlabel('Update Number')
+plt.grid(True)
+plt.xlim([0, 300])
+
+plot4 = plt.figure(4)
+plt.plot(t[0], t[7])
+plt.plot(t[0], t[8])
+plt.ylabel('Gain')
+plt.xlabel('Update Number')
+plt.grid(True)
+plt.xlim([0, 100])
+plt.legend(['Position Gain', 'Velocity Gain'])
+plt.title('Position and Velocity Gains \n', fontweight="bold")
+plt.show()
